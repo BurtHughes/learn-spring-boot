5c97fefc65f92f23e1e3b29f988a103d5434c604
Burt Hughes <normanthompson@163.com>
2018-05-07 22:31:32 +0800

    整合freemarker，未测试通过

---
+++ src/main/java/com/penguin/learn/learnspringboot/web/FreemarkerController.java
@@ -0,0 +1,16 @@
+package com.penguin.learn.learnspringboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("freemarker")
+public class FreemarkerController {
+    @RequestMapping("hello")
+    public String hello(Map<String, Object> map){
+        map.put("msg", "Hello Freemarker");
+        return "hello";
+    }
+}
