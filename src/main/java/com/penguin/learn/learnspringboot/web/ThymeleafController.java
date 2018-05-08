533927069f02c9a4435a1b88eb1e61aaba04cb07
Burt Hughes <normanthompson@163.com>
2018-05-08 21:29:34 +0800

    整合Thymeleaf

---
+++ src/main/java/com/penguin/learn/learnspringboot/web/ThymeleafController.java
@@ -0,0 +1,16 @@
+package com.penguin.learn.learnspringboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("thymeleaf")
+public class ThymeleafController {
+    @RequestMapping("hello")
+    public String hello(Map<String, Object> map){
+        map.put("msg", "Hello Thymeleaf");
+        return "hello";
+    }
+}
