58289c71a62854aed415b1d9d34ff8997ae93c1b
Burt Hughes <normanthompson@163.com>
2018-05-08 22:44:54 +0800

    添加自定义过滤器

--- src/main/java/com/penguin/learn/learnspringboot/config/WebConfig.java
+++ src/main/java/com/penguin/learn/learnspringboot/config/WebConfig.java
@@ -3,12 +3,17 @@ package com.penguin.learn.learnspringboot.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.penguin.learn.learnspringboot.filter.TimeFilter;
 import com.penguin.learn.learnspringboot.servlet.ServletTest;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class WebConfig {
@@ -30,5 +35,23 @@ public class WebConfig {
     @Bean
     public ServletRegistrationBean servletRegistrationBean() {
         return new ServletRegistrationBean(new ServletTest(),"/servletTest");
+    }
+
+    /**
+     * 添加到过滤器链，适合第三方过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean timeFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+
+        TimeFilter timeFilter = new TimeFilter();
+        registrationBean.setFilter(timeFilter);
+
+        List<String> urls = new ArrayList<>();
+        urls.add("/*");
+        registrationBean.setUrlPatterns(urls);
+
+        return registrationBean;
     }
 }
---
+++ src/main/java/com/penguin/learn/learnspringboot/filter/TimeFilter.java
@@ -0,0 +1,30 @@
+package com.penguin.learn.learnspringboot.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * 1、Component注解直接将过滤器交给Spring容器管理，适合自定义过滤器
+ * 2、在WebConfig中添加Bean，将自定义过滤器添加到过滤器链，适合第三方过滤器
+ * 二者选择其一
+ */
+//@Component
+public class TimeFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("=======初始化过滤器=========");
+    }
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        long start = System.currentTimeMillis();
+        filterChain.doFilter(request, response);
+        System.out.println("filter 耗时：" + (System.currentTimeMillis() - start));
+    }
+    @Override
+    public void destroy() {
+        System.out.println("=======销毁过滤器=========");
+    }
+}
