40ccb18e72c9323da8f3ded4e56e869b48611989
Burt Hughes <normanthompson@163.com>
2018-05-28 22:50:22 +0800

    集成MyBatis

---
+++ src/main/java/com/penguin/learn/springboot/mapper/DepartmentMapper.java
@@ -0,0 +1,14 @@
+package com.penguin.learn.springboot.mapper;
+
+import com.penguin.learn.springboot.pojo.Department;
+
+public interface DepartmentMapper {
+
+    public void insert(Department department);
+
+    public Department getById(Integer id);
+
+    public void update(Department department);
+
+    public void deleteById(Integer id);
+}
